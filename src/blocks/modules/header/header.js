b3686ff66a71ff796e766a8506aba593a748484d
Boris <sslonovborisss@gmail.com>
2020-10-23 14:53:02 +0300

    add page process

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -16,3 +16,36 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+//slowScroll
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
+
+
+//add class on scroll
+document.addEventListener("scroll", (e) => {
+
+    const content = document.querySelector("body");
+    let scrolled = document.scrollingElement.scrollTop;
+    let position = content.offsetTop;
+    let header = document.querySelector("header");
+
+    if (scrolled > position + 50) {
+        content.classList.add('header-bgc');
+    } else {
+        content.classList.remove('header-bgc');
+        content.classList.add('header-none-bgc');
+    }
+});
