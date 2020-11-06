ee98d03bc033896486f673223b94644734ac0954
Boris <sslonovborisss@gmail.com>
2020-11-06 16:07:24 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,14 +1,14 @@
 <?php 
 
-var_dump($_POST);
+// var_dump($_POST);
 
-$convertedText = mb_convert_encoding($msg, 'utf-8', mb_detect_encoding($msg));
+$convertedText = mb_convert_encoding($txt, 'utf-8', mb_detect_encoding($txt));
 
 
 $token = "1437004960:AAFNz7URg5eyKN1aMcweP8VsTVp6H5PbPbw";
 $chat_id = "-400261710";
-$msg = json_encode($_POST);
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$msg}","r");
+// $msg = json_encode($_POST);
+
 if ($_POST['act'] == 'order') {
     $name = ($_POST['name']);
     $email = ($_POST['email']);
@@ -24,6 +24,8 @@ if ($_POST['act'] == 'order') {
     foreach($arr as $key => $value) {
         $txt .= "<b>".$key."</b> ".$value."%0A";
     }
+
+    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 
   //Если сообщение отправлено, напишет "Thank you", если нет - "Error"
