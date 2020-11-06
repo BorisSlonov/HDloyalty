3411d1f22382247592a642330a59e8f2ddcbb57d
Boris <sslonovborisss@gmail.com>
2020-11-06 15:59:43 +0300

    Update send.php

--- send.php
+++ send.php
@@ -4,9 +4,8 @@ var_dump($_POST);
 
 $token = "1437004960:AAFNz7URg5eyKN1aMcweP8VsTVp6H5PbPbw";
 $chat_id = "-400261710";
-
 $msg = json_encode($_POST);
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text=text","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$msg}","r");
 if ($_POST['act'] == 'order') {
     $name = ($_POST['name']);
     $email = ($_POST['email']);
